61eb44636ae691a45be11414a0ad2c49148b8b4b
baihaqism <baihaqi.bsm@gmail.com>
2023-12-14 17:24:07 +0700

    feat: Update aboutData, About, and tailwind config
    - Modified src/data/aboutData.ts to update the skills and tools data.
    - Modified src/pages/About.tsx to add functionality for toggling between skills and tools sections.
    - Modified tailwind.config.js to adjust styling classes.

--- src/data/aboutData.ts
+++ src/data/aboutData.ts
@@ -1,29 +1,93 @@
-export const aboutData = {
-  title: "About Me",
-  description: "I'm a passionate software developer with a love for creating efficient and scalable solutions. My expertise includes:",
-  skills: [
-    {
-      name: "JavaScript",
-      level: "Advanced"
-    },
-    {
-      name: "TypeScript",
-      level: "Advanced"
-    },
-    {
-      name: "React",
-      level: "Advanced"
-    },
-    {
-      name: "Node.js",
-      level: "Advanced"
-    },
-    // Add more skills as needed
-  ]
-};
+import { SiTypescript, SiJavascript, SiPython, SiCplusplus, SiHtml5, SiCss3, SiVite, SiReact, SiTailwindcss, SiNodedotjs, SiExpress } from "react-icons/si";
+import { FaJava, FaGit } from "react-icons/fa6";
 
-export const programmingLanguages = ['TypeScript', 'JavaScript', 'Python', 'Java', 'C++'];
-export const backendDevelopment = ['Node.js', 'Express', 'MySQL'];
-export const frontendDevelopment = ['HTML', 'CSS', 'Vite', 'React', 'Tailwind CSS'];
-export const developmentTools = ['VSCode', 'Git'];
-export const deploymentHosting = ['Vercel', 'Railway'];
+export interface skillsData {
+  name: string;
+  level: string;
+  icon: React.ComponentType;
+  color?: string;
+}
+
+export const skillsData: skillsData[] = [
+  {
+    name: "HTML",
+    level: "Advanced",
+    icon: SiHtml5,
+    color: "text-orange-500"
+  }, 
+  {
+    name: "CSS",
+    level: "Advanced",
+    icon: SiCss3,
+    color: "text-blue-500"
+  },
+  {
+    name: "TypeScript",
+    level: "Advanced",
+    icon: SiTypescript,
+    color: "text-blue-500"
+  },
+  {
+    name: "JavaScript",
+    level: "Advanced",
+    icon: SiJavascript,
+    color: "text-yellow-400"
+  },
+  {
+    name: "Python",
+    level: "Advanced",
+    icon: SiPython,
+    color: "text-yellow-400"
+  },
+  {
+    name: "Java",
+    level: "Advanced",
+    icon: FaJava,
+    color: "text-red-500"
+  },
+  {
+    name: "C++",
+    level: "Advanced",
+    icon: SiCplusplus,
+    color: "text-blue-400"
+  },
+  {
+    name: "Vite",
+    level: "Advanced",
+    icon: SiVite,
+    color: "text-purple-500"
+  },
+  {
+    name: "React",
+    level: "Advanced",
+    icon: SiReact,
+    color: "text-blue-500"
+  },
+  {
+    name: "Tailwind CSS",
+    level: "Advanced",
+    icon: SiTailwindcss,
+    color: "text-blue-500"
+  },
+  {
+    name: "Node.js",
+    level: "Advanced",
+    icon: SiNodedotjs,
+    color: "text-green-500"
+  },
+  {
+    name: "Express",
+    level: "Advanced",
+    icon: SiExpress,
+    color: "text-white"
+  },
+];
+
+export const toolsData: skillsData[] = [
+  {
+    name: "Git",
+    level: "Advanced",
+    icon: FaGit,
+    color: "text-red-500"
+  },
+];
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,29 +1,27 @@
 import React, { useState } from "react";
+import { skillsData, toolsData } from "../data/aboutData";
 import profileImage from "../assets/photoprofile.jpg";
-import {
-  programmingLanguages,
-  backendDevelopment,
-  frontendDevelopment,
-  developmentTools,
-  deploymentHosting,
-} from "../data/aboutData";
 
 const About: React.FC = () => {
-  const [selectedCategory, setSelectedCategory] = useState<string>("");
+  const [showSkills, setShowSkills] = useState(true);
+  const [showTools, setShowTools] = useState(false);
 
-  const handleCategoryClick = (category: string) => {
-    setSelectedCategory(category);
+  const toggleSkills = () => {
+    if (!showSkills) {
+      setShowSkills(true);
+      setShowTools(false);
+    }
   };
 
-  const renderSkills = (skills: string[]) => {
-    return (
-      <ul>
-        {skills.map((skill) => (
-          <li key={skill}>{skill}</li>
-        ))}
-      </ul>
-    );
+  const toggleTools = () => {
+    if (!showTools) {
+      setShowTools(true);
+      setShowSkills(false); 
+    }
   };
+
+  const active = "inline-block px-4 py-3 rounded-lg hover:text-white text-accent font-bold bg-accent bg-opacity-10";
+  const inactive = "inline-block px-4 py-3 font-bold rounded-lg hover:text-white";
 
   return (
     <div className="mt-10 mb-10 md:mt-1 p-4">
@@ -45,87 +43,70 @@ const About: React.FC = () => {
       </div>
       <div className="mt-6">
         <h2 className="text-3xl font-bold mb-4">Skills</h2>
-        <div className="grid grid-cols-2 gap-4 pb-32 md:grid-cols-3 md:gap-8 xl:grid-cols-4 xl:gap-10 2xl:gap-12">
-          <div>
-            <h3 className="text-xl font-bold mb-2">
-              Programming Languages
-              <button
-                className={`ml-2 text-blue-500 focus:outline-none ${
-                  selectedCategory === "Programming Languages"
-                    ? "font-bold"
-                    : ""
-                }`}
-                onClick={() => handleCategoryClick("Programming Languages")}
-              >
-                Show
-              </button>
-            </h3>
-            {selectedCategory === "Programming Languages" &&
-              renderSkills(programmingLanguages)}
+        <div className="flex flex-wrap text-sm font-large text-center text-gray-500 mb-5">
+          <div className="mr-2">
+            <button
+              className={showSkills ? active : inactive}
+              onClick={toggleSkills}
+            >
+              Tech Stack
+            </button>
           </div>
           <div>
-            <h3 className="text-xl font-bold mb-2">
-              Backend Development
-              <button
-                className={`ml-2 text-blue-500 focus:outline-none ${
-                  selectedCategory === "Backend Development" ? "font-bold" : ""
-                }`}
-                onClick={() => handleCategoryClick("Backend Development")}
-              >
-                Show
-              </button>
-            </h3>
-            {selectedCategory === "Backend Development" &&
-              renderSkills(backendDevelopment)}
-          </div>
-          <div>
-            <h3 className="text-xl font-bold mb-2">
-              Frontend Development
-              <button
-                className={`ml-2 text-blue-500 focus:outline-none ${
-                  selectedCategory === "Frontend Development" ? "font-bold" : ""
-                }`}
-                onClick={() => handleCategoryClick("Frontend Development")}
-              >
-                Show
-              </button>
-            </h3>
-            {selectedCategory === "Frontend Development" &&
-              renderSkills(frontendDevelopment)}
-          </div>
-          <div>
-            <h3 className="text-xl font-bold mb-2">
-              Development Tools
-              <button
-                className={`ml-2 text-blue-500 focus:outline-none ${
-                  selectedCategory === "Development Tools" ? "font-bold" : ""
-                }`}
-                onClick={() => handleCategoryClick("Development Tools")}
-              >
-                Show
-              </button>
-            </h3>
-            {selectedCategory === "Development Tools" &&
-              renderSkills(developmentTools)}
-          </div>
-          <div>
-            <h3 className="text-xl font-bold mb-2">
-              Deployment and Hosting
-              <button
-                className={`ml-2 text-blue-500 focus:outline-none ${
-                  selectedCategory === "Deployment and Hosting"
-                    ? "font-bold"
-                    : ""
-                }`}
-                onClick={() => handleCategoryClick("Deployment and Hosting")}
-              >
-                Show
-              </button>
-            </h3>
-            {selectedCategory === "Deployment and Hosting" &&
-              renderSkills(deploymentHosting)}
+            <button
+              className={showTools ? active : inactive}
+              onClick={toggleTools}
+            >
+              Tools
+            </button>
           </div>
         </div>
+        {showSkills && (
+          <div className="grid grid-cols-2 gap-4 pb-16 md:grid-cols-3 md:gap-8 xl:grid-cols-4 xl:gap-10 2xl:gap-12">
+            {skillsData.map((skill, index) => (
+              <div key={index}>
+                <div className="group flex cursor-pointer items-center gap-2 rounded border border-accent px-2 py-2 hover:bg-accent hover:bg-opacity-10 md:gap-3 lg:px-3">
+                  <div className="flex h-12 w-12 items-center justify-center lg:h-16 lg:w-16 lg:p-2 duration-300 group-hover:scale-150">
+                    {React.cloneElement(<skill.icon />, {
+                      className: skill.color,
+                    })}
+                  </div>
+                  <div className="flex items-center text-sm md:text-base lg:text-lg relative">
+                    <div className="font-medium text-secondary translate-y-0 transition-all duration-300 group-hover:-translate-y-2">
+                      {skill.name}
+                    </div>
+                    <div className="opacity-0 absolute mt-5 text-xs text-accent transition-all duration-300 md:text-sm lg:text-base group-hover:opacity-100">
+                      {skill.level}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+        {showTools && (
+          <div className="grid grid-cols-2 gap-4 pb-16 md:grid-cols-3 md:gap-8 xl:grid-cols-4 xl:gap-10 2xl:gap-12">
+            {toolsData.map((tool, index) => (
+              <div key={index}>
+                <div className="group flex cursor-pointer items-center gap-2 rounded border border-accent px-2 py-2 hover:bg-accent hover:bg-opacity-10 md:gap-3 lg:px-3">
+                  <div className="flex h-12 w-12 items-center justify-center lg:h-16 lg:w-16 lg:p-2 duration-300 group-hover:scale-150">
+                    {React.cloneElement(<tool.icon />, {
+                      className: tool.color,
+                    })}
+                  </div>
+                  <div className="flex items-center text-sm md:text-base lg:text-lg relative">
+                    <div className="font-medium text-secondary translate-y-0 transition-all duration-300 group-hover:-translate-y-2">
+                      {tool.name}
+                    </div>
+                    <div className="opacity-0 absolute mt-5 text-xs text-accent transition-all duration-300 md:text-sm lg:text-base group-hover:opacity-100">
+                      {tool.level}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
